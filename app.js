c61844940dec7531f78e254b8dff8ea46b2e8008
Harshit Kumar <harshitkumarhk1290@gmail.com>
2025-03-18 15:04:48 +0530

    Added some part of error handling mechanism

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
+const path = require('path');
+const ExpressError = require('./utils/ExpressError.js');
 
 async function connectDB() {
     await mongoose.connect(process.env.DB_URL)
@@ -15,4 +17,27 @@ connectDB()
 })
 .catch((err)=>{
     console.log(err);
+});
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/test",(req,res)=>{
+    res.send("Server is running");
+});
+
+app.all("*", (req, res, next) => {
+    next(new ExpressError(404, "Page Not Found"));
 })
+
+app.use((err, req, res, next) => {
+    let { status = 500, message = "Some error occurred" } = err;
+    res.status(status).render("error.ejs", { err })
+});
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+});
